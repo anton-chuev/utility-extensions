f82660645e632d56d531b3d2c08199f81b96eaad
Anton Chuev <anton.chuev@applitools.com>
2024-01-31 12:04:29 +0200

    Initial Commit

---
+++ Tests/UtilityExtensionsTests/UtilityExtensionsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import UtilityExtensions
+
+final class UtilityExtensionsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
