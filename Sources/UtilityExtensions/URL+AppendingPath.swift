aa3940689776594798c3e131722d1eab1e1642ec
Anton Chuev <anton.chuev@applitools.com>
2024-01-31 13:21:33 +0200

    Renamed method in URL extension
    To make it different from the system one

--- Sources/UtilityExtensions/URL+AppendingPath.swift
+++ Sources/UtilityExtensions/URL+AppendingPath.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension URL {
-    func appending(_ pathComponent: String) -> URL {
+    func appendingComponent(_ pathComponent: String) -> URL {
         if #available(iOS 16.0, *) {
             return appending(component: pathComponent)
         } else {
